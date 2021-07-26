8912fb8d6342fa2e196a23fcbb15f80a57b2d49e
enrico-dgr <elcidwar@gmail.com>
2021-07-27 01:40:14 +0200

    fixed for prod

--- webpack.config-prod.js
+++ webpack.config-prod.js
@@ -1,11 +1,11 @@
 var path = require("path");
 
-/**
- *
- */
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   watch: false,
   mode: "production",
+  devtool: "source-map",
   entry: {
     preload: path.resolve(__dirname, "./src/index.ts"),
     style: path.resolve(__dirname, "./src/style.ts"),
@@ -18,6 +18,11 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -26,11 +31,21 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: [
           "style-loader",
           "css-loader",
-
+          {
+            loader: "resolve-url-loader",
+          },
           {
             loader: "sass-loader",
             options: {
